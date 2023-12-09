d72cea41e6f88adaf14a5d2f1a337ae12b0cefeb
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-12-09 14:29:01 +0530

    admin manage book back-end

--- api/urls_admin.py
+++ api/urls_admin.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views_admin import getUsers
+from .views_admin import getUsers , changeUserStatus
 
 urlpatterns = [
     path('users', getUsers.as_view()),
+    path('users/update', changeUserStatus.as_view()),
 
 ]
--- api/views_admin.py
+++ api/views_admin.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
+from django.db.models import Q
 from . models import User
 from . serializers_admin import UserAccountSerializer
+from django.core.paginator import Paginator
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.decorators import  permission_classes
@@ -11,8 +13,38 @@ class getUsers(APIView):
     def post(self,request):
         if request.user.is_superuser:
             active = request.data["status"]
-            users = User.objects.filter(is_superuser=0 , is_active=(active == "active"))
-            serializer = UserAccountSerializer(users , many = True)
-            return Response(serializer.data)
+            page = request.data["page"]
+            search = None;
+            if "search" not in request.data or request.data["search"] == "":
+                users = User.objects.filter(is_superuser=0 , is_active=(active == "active")).order_by("name")
+            else:
+                search = request.data["search"]
+                users = User.objects.filter((Q(name__icontains=search) | Q(email__icontains=search)) & Q(is_superuser=0)).order_by("name")
+
+            paginator = Paginator(users, 25)
+            paginated_users = paginator.get_page(page)
+            serializer = UserAccountSerializer(paginated_users , many = True)
+            return Response({"users":serializer.data , 
+                            "meta":{
+                                "count":paginator.count , 
+                                "page_count":paginator.num_pages,
+                                "start":paginated_users.start_index(),
+                                "end":paginated_users.end_index()
+                                }
+                            })
+        else:
+            raise PermissionDenied({"details":"You don't have permission to this action."})
+        
+@permission_classes([IsAuthenticated])
+class changeUserStatus(APIView):
+    def post(self,request):
+        if request.user.is_superuser:
+            active = request.data["status"]
+            users = User.objects.get(email = request.data["email"])
+            users.is_active = (active == "active")
+            users.save()
+            return Response({
+                "details":"success"
+            })
         else:
             raise PermissionDenied({"details":"You don't have permission to this action."})
